6fbe453ab8193c0e0243822a87406f5fa635aeec
guobaozhi <gbz1022@hotmail.com>
2026-03-15 13:24:14 +0800

    update some config

---
+++ public/js/go-comment.js
@@ -0,0 +1,16 @@
+// go-comment.js
+document.addEventListener('scroll', function () {
+    const comment = document.getElementById("comments-title");
+    const bottomToComment = document.getElementById("comments-link")
+    const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    const offsetTop = comment.offsetTop
+    const scrollTop = document.documentElement.scrollTop
+    const top = offsetTop - scrollTop
+if (top <= viewPortHeight + 100) {
+    bottomToComment.style.visibility = "hidden";
+    bottomToComment.style.opacity = "0";
+} else {
+    bottomToComment.style.visibility = "visible";
+    bottomToComment.style.opacity = "1";
+}
+})
---
+++ public/js/go-top.js
@@ -0,0 +1,29 @@
+// go-top.js
+let menu = document.getElementById('menu')
+if (menu) {
+menu.scrollLeft = Number(localStorage.getItem("menu-scroll-position"));
+menu.onscroll = function () {
+localStorage.setItem("menu-scroll-position", menu.scrollLeft.toString());
+}
+}
+
+
+
+window.onscroll = function () {
+const goTopButton = document.getElementById("top-link");
+if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+goTopButton.style.visibility = "visible";
+goTopButton.style.opacity = "1";
+} else {
+goTopButton.style.visibility = "hidden";
+goTopButton.style.opacity = "0";
+}
+};
+
+document.addEventListener('scroll', function (e) {
+const readProgress = document.getElementById("pe-read-progress");
+const scrollHeight = document.documentElement.scrollHeight;
+const clientHeight = document.documentElement.clientHeight;
+const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+readProgress.innerText = ((scrollTop / (scrollHeight - clientHeight)).toFixed(2) * 100).toFixed(0);
+})
